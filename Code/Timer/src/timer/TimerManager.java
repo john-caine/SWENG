25c686e23ea7e1127645d1200f80e03a2d6c7d77
St659 <st659@york.ac.uk>
2014-04-28 19:39:14 +0100

    Timer Manager alterations
    Further testing and editing of timer manager class. Nothing conclusive
    at this point, still locks up after creating first instance of timer.

--- Code/Timer/src/timer/TimerManager.java
+++ Code/Timer/src/timer/TimerManager.java
@@ -1,5 +1,6 @@
 package timer;
 
+import javafx.application.Platform;
 import javafx.concurrent.WorkerStateEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
@@ -12,40 +13,58 @@ public class TimerManager {
 	private Timer timer;
 	private int numberOfTimers = 0;
 	private Button timerManagerlabel;
+	private Timer timer2;
 
 	public TimerManager(Group group){
 		
 		timerManagerlabel = new Button("Create timer");
-		group.getChildren().add(timerManagerlabel);
 		
+				group.getChildren().add(timerManagerlabel);
+		
+		
+		System.out.println(group.getChildren().size());
 		timerManagerlabel.setOnMouseClicked(new EventHandler<MouseEvent>(){
+
+		
 
 			@Override
 			public void handle(MouseEvent event) {
 				
-				System.out.println("Timer created");
+			
 				timer = new Timer();
 				 
 				 timer.setOnSucceeded(new EventHandler<WorkerStateEvent>(){
+
+					
 
 					@Override
 					public void handle(WorkerStateEvent event) {
 						System.out.println("Success!");
 						timer.getPane().setLayoutX(timer.getPane().getLayoutX() + timer.getPane().getWidth()*numberOfTimers);
-						group.getChildren().add(timer.getPane());
+						
+						Platform.runLater( new Runnable(){
+							public void run(){
+								group.getChildren().add(timer.getPane());
+							}
+						});	 
+						
 						numberOfTimers++;
-						System.out.println(event.isConsumed());
-						event.consume();
-						System.out.println(event.isConsumed());
+						System.out.println(numberOfTimers);
+						System.out.println(group.getChildren().size());
+						timer2 = new Timer();
+				
+						
 						
 					}
 					 
 				 });
 				
-				 new Thread(timer).start();
+				new Thread(timer).start();
+				
 				 System.out.println("I am here");
-				 
+				 event.consume();
 				System.out.println(event.isConsumed());
+				
 			}
 			
 			
