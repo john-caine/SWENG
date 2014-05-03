aee8686baa2dc329063b8373375e5a622657b11a
zt530 <zt530@york.ac.uk>
2014-05-03 15:29:44 +0100

    Recipe Screen

--- Code/GUI/src/gui/RecipeScreen.java
+++ Code/GUI/src/gui/RecipeScreen.java
@@ -6,6 +6,8 @@ import java.io.InputStream;
 
 import javafx.animation.FadeTransition;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
@@ -26,10 +28,6 @@ public class RecipeScreen {
 	
 	
 	public RecipeScreen(final VBox bigBox, double height, double width){
-		
-		/////////////////////////////////////////////////////////////////
-		//RS1: TOP
-		HBox topBox = new HBox(100);
 		logoHolder1 = new ImageView();
 		try {
 			inputStream = new FileInputStream("../Resources/SpoonSmall.png");
@@ -49,44 +47,57 @@ public class RecipeScreen {
 		} 
 		image2 = new Image(inputStream);
 		logoHolder2.setImage(image2);
-//		topBox.getChildren().add(logoHolder1);
-//		topBox.getChildren().add(logoHolder2);
 		
-		logoHolder1.setLayoutX(0);
-		logoHolder1.setLayoutY(0);
+		HBox topBox = new HBox(width/2 - image1.getWidth() - (image2.getWidth()/2));
+		topBox.setPrefHeight(height/10);
+		topBox.getChildren().add(logoHolder1);
+		topBox.getChildren().add(logoHolder2);
+         
 		
-		logoHolder1.setLayoutX(width/2);
-		logoHolder1.setLayoutY(height/2);
-		/////////////////////////////////////////////////////////////////
-		//RS2: MID LEFT: CHOICES (i.e. NEWEST, $5 MEALS, UNDER 5 MINUTES ETC)
-		VBox midBoxLeft = new VBox(10);
+	
+		
+		VBox midBoxLeft = new VBox(20);
+		midBoxLeft.setPadding(new Insets(20, 20, 20, 20));
+		midBoxLeft.setPrefSize((width/2) - 10, height- topBox.getPrefHeight() - 80);
+		
+		HBox midBoxRight = new HBox();
+		midBoxRight.setPrefSize((width/2) - 10, height- topBox.getPrefHeight() - 80);
+		System.out.println("top height: " + topBox.getPrefHeight());
+		
+		HBox midBox = new HBox();
+		midBox.setPrefWidth(width);
+		midBox.setPrefHeight(height - topBox.getPrefHeight());
+		
+		System.out.println("midBox height: " + midBox.getPrefHeight() + " midBox width: " + midBox.getPrefWidth());
+		System.out.println("midLeft height: " + midBoxLeft.getPrefHeight() + " midLeft width: " + midBoxLeft.getPrefWidth());
+		System.out.println("midRight height: " + midBoxRight.getPrefHeight() + " midRight width: " + midBoxRight.getPrefWidth());
+		
 		Button myRecipesBtn = new Button("My Recipes");
 		Button newestBtn = new Button("Newest Meals");
 		Button fifteenMinMealBtn = new Button("15 Min Meals");
 		Button underFiveBtn = new Button("Under $5");
 		Button inSeasonBtn = new Button("In Season");
-		
-		
-		/////////////////////////////////////////////////////////////////
-		//RS2.1: BUTTON LAYOUT
+				
 		myRecipesBtn.setId("b5");
 		newestBtn.setId("b6");
 		fifteenMinMealBtn.setId("b7");
 		underFiveBtn.setId("b8");
 		inSeasonBtn.setId("b9");
-		
-		myRecipesBtn.setMinSize(300, 68);
-		newestBtn.setMinSize(300, 68);
-		fifteenMinMealBtn.setMinSize(300, 68);
-		underFiveBtn.setMinSize(300, 68);
-		inSeasonBtn.setMinSize(300, 68);
-		
+				
+		myRecipesBtn.setPrefSize(width/2 - 5, midBox.getPrefHeight()/6 - 30);
+		newestBtn.setPrefSize(width/2 - 5, midBox.getPrefHeight()/6 - 30);
+		fifteenMinMealBtn.setPrefSize(width/2 - 5, midBox.getPrefHeight()/6 - 30);
+		underFiveBtn.setPrefSize(width/2 - 5, midBox.getPrefHeight()/6 - 30);
+		inSeasonBtn.setPrefSize(width/2 - 5, midBox.getPrefHeight()/6 - 30);
+				
 		myRecipesBtn.getStylesheets().add("css.css");
 		newestBtn.getStylesheets().add("css.css");
 		fifteenMinMealBtn.getStylesheets().add("css.css");
 		underFiveBtn.getStylesheets().add("css.css");
 		inSeasonBtn.getStylesheets().add("css.css");
 
+		
+		
 		midBoxLeft.getChildren().add(myRecipesBtn);
 		midBoxLeft.getChildren().add(newestBtn);
 		midBoxLeft.getChildren().add(fifteenMinMealBtn);
@@ -94,23 +105,19 @@ public class RecipeScreen {
 		midBoxLeft.getChildren().add(inSeasonBtn);
 		
 		
-		/////////////////////////////////////////////////////////////////
-		//RS3: MID RIGHT: RECIPE LIST 
+		
 		ScrollPane recipeList = new ScrollPane(); //recipeList is the scroll panel
 		recipeList.setStyle("-fx-background: lightgrey;");
-		recipeList.setMinWidth(330);
-		recipeList.setMaxHeight(380);
+		recipeList.setPrefSize((width/2) - 10, height- topBox.getPrefHeight() - 80);
 		
 		VBox listContent = new VBox(10);  //content of the scroll panel
-		listContent.setMinWidth(recipeList.getMinWidth() - 20);
+		listContent.setPrefWidth(recipeList.getPrefWidth() - 20);
 		recipeList.setContent(listContent);
 		
 		
-		/////////////////////////////////////////////////////////////////
-		//RS3.1: CHECKING IF LABELS CAN BE MADE TO DO THINGS UPON CLICKING
+		
 		Label testLabel = new Label("Test"); 
-		testLabel.setMinHeight(50);
-	    testLabel.setMinWidth(listContent.getMinWidth());
+		testLabel.setPrefSize((width/2) - 10, height- topBox.getPrefHeight() - 80);
 	    testLabel.setStyle("-fx-border-color:black; -fx-background-color: white;");
 		testLabel.setOnMouseClicked(new EventHandler<MouseEvent>(){
 
@@ -123,44 +130,29 @@ public class RecipeScreen {
 		listContent.getChildren().add(testLabel);
 		
 		
-		////////////////////////////////////////////////////////////////
-		//RS3.2: MAKING MULTIPLE LABELS
 		for (int i = 0; i < 50; i++)
 		{
 		    Label tempList = new Label("Label " + i); //tempList = temporary list; recipes that make up the content
 		    
-		    tempList.setMinHeight(50);
-		    tempList.setMinWidth(listContent.getMinWidth());
+		    tempList.setPrefHeight(50);
+		    tempList.setPrefWidth(listContent.getMinWidth());
 		    tempList.setStyle("-fx-border-color:pink; -fx-background-color: lightblue;");
 		    
 		    listContent.setPrefHeight(recipeList.getPrefHeight() + tempList.getPrefHeight());
-		    recipeList.setMinHeight(100);
+		    recipeList.setPrefHeight(100);
 		    listContent.getChildren().add(tempList);
 		    
 			
 		    
 		}
 		
-		HBox midBoxRight = new HBox();
+		
 		midBoxRight.getChildren().add(recipeList);
 		
-		midBoxRight.setLayoutY(300);
-		
-		
-		/////////////////////////////////////////////////////////////////
-		//RS5: FADE EFFECT
-		FadeTransition fadeTransition 
-        = new FadeTransition(Duration.millis(1000), bigBox);
-		 fadeTransition.setFromValue(0.0);
-		 fadeTransition.setToValue(1.0);
-		 fadeTransition.play();
-		bigBox.setVisible(true);
-		
-		bigBox.getChildren().addAll(logoHolder1,logoHolder2);
+		midBox.setAlignment(Pos.CENTER);
+		midBox.getChildren().addAll(midBoxLeft,midBoxRight);
 		bigBox.getChildren().add(topBox);
-		bigBox.getChildren().add(midBoxRight);
-
-              
+		bigBox.getChildren().add(midBox);
 	}
 	 
 }
