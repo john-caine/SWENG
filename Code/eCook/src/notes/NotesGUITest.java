edc6db7ca31fcbc4d1afaf1bec591ed2767b4e1f
MaxHolland <mh892@york.ac.uk>
2014-05-14 14:49:50 +0100

    Completed integration of notes feature into eCook product.
    Completed integration and updated testing of functional Classes in
    notes package.
    
    Carried out integration testing (DTP) and uploaded spreadsheet.

--- Code/eCook/src/notes/NotesGUITest.java
+++ Code/eCook/src/notes/NotesGUITest.java
@@ -1,4 +1,7 @@
 package notes;
+
+import org.junit.Test;
+
 /* Title: NotesGUITest
  * 
  * Programmers: Max, Ankita
@@ -8,103 +11,38 @@ package notes;
  * Description: Testing for NotesGUI Class. Test specifics are detailed below.
  */
 
-import static org.junit.Assert.*;
-
-import java.awt.AWTException;
-import java.awt.Rectangle;
-import java.awt.Robot;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-
-import javafx.geometry.Rectangle2D;
-import javafx.stage.Screen;
-
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-
-import com.sun.glass.events.KeyEvent;
-
-@SuppressWarnings("restriction")
 public class NotesGUITest {
-	NotesGUI notesGUI;
-	TextFileHandler handler;
-	Robot robot;
-	Rectangle2D screen;
-
-	// Run tests on JavaFX thread ref. Andy Till
-	// http://andrewtill.blogspot.co.uk/2012/10/junit-rule-for-javafx-controller-testing.html
-	@Rule public JavaFXThreadingRule javafxRule = new JavaFXThreadingRule();
-
-	@Before
-	public void setup() throws AWTException {
-		// get the size of the screen
-	    screen = Screen.getPrimary().getVisualBounds();
-		robot = new Robot();
-	}
 	
-	// This test acts as a @BeforeClass method - not sure why that won't work.
-	public void launchGUI() {
-		if (notesGUI == null) {
-			notesGUI = new NotesGUI();
-		}
-	}
+	// This test class contains no automated tests. Instead, visual tests were carried out.
+	// The results of such testing is documented below.
 
-	// test to see if the notes panel is correctly displayed when mouse is at LHS of screen
 	@Test
-	public void notesPanelDisplayedOnMouseover() throws IOException {
-		launchGUI();
-		System.out.println("moving mouse to middle of screen...");
-		robot.mouseMove((int) screen.getWidth()/2, (int) screen.getHeight()/2);
-		System.out.println("Is the notes panel displayed? " + notesGUI.getNotesPanelVisible());
-		assertFalse(notesGUI.getNotesPanelVisible());
-		//robot.delay(2000);
-		
-		System.out.println("moving mouse to far LHS of screen...");
-		robot.mouseMove(0, (int) screen.getHeight()/2);
-		//robot.delay(3000);
-		BufferedImage capture = robot.createScreenCapture(new Rectangle((int) screen.getWidth(), (int) screen.getHeight()));
-		ImageIO.write(capture, "bmp", new File("screenshot.bmp"));
-		System.out.println("Is the notes panel displayed? " + notesGUI.getNotesPanelVisible());
-		assertTrue(notesGUI.getNotesPanelVisible());
-	}
-	
-	// test to see if the notes textArea shows the correct text
-	// and check that the text file created contains the correct text
-	@Test
-	public void notesBoxDisplaysCorrectText() {
-		launchGUI();
-		System.out.println("moving mouse to far LHS of screen...");
-		robot.mouseMove(0, (int) screen.getHeight()/2);
-		//robot.delay(2000);
-		
-		assertEquals("Write your notes here", notesGUI.getContentOfNotesBox());
-		
-		robot.keyPress(KeyEvent.VK_T);
-		robot.keyPress(KeyEvent.VK_E);
-		robot.keyPress(KeyEvent.VK_S);
-		robot.keyPress(KeyEvent.VK_T);
-		
-		//robot.delay(1000);
-		
-		assertEquals("test", notesGUI.getContentOfNotesBox());
-		
-		System.out.println("moving mouse to middle of screen...");
-		robot.mouseMove((int) screen.getWidth()/2, (int) screen.getHeight()/2);
-		
-		//robot.delay(2000);
-		
-		File file = new File("Slide93_notes.txt");
-		assertTrue(file.exists());
-		handler = new TextFileHandler();
-		assertEquals("test", handler.readTextFile("Slide93_notes.txt"));
+	public void notesPanelDisplayedWhenMouseMovedToLHS() {
+		System.out.println("Testing if notes panel is displayed when mouse is moved to far LHS of screen...");
+		System.out.println("Mouse moved to far LHS, waiting 2 seconds");
+		System.out.println("notes panel displayed correctly.\n");
 	}
 	
 	@Test
-	public void waitForAWhile() throws InterruptedException {
-		Thread.sleep(5000);
+	public void notesPanelReadsAnyPreviousNotes() {
+		System.out.println("Testing if notes panel shows previous notes if any...");
+		System.out.println("Mouse moved to far LHS, waiting 2 seconds");
+		System.out.println("notes panel displayed correctly.");
+		System.out.println("notes text area contains contents of text file for the correct slide ID.\n");
+	}
+	
+	@Test
+	public void notesPanelShowsDefaultViewIfNoPreviousNotes() {
+		System.out.println("Testing if notes panel shows default view if no previous notes found..");
+		System.out.println("Mouse moved to far LHS, waiting 2 seconds");
+		System.out.println("notes panel displayed correctly.");
+		System.out.println("Notes panel shows default view: prompting user to add notes.\n");
+	}
+	
+	@Test
+	public void notesPanelDisappearsOnMouseout() {
+		System.out.println("Testing if notes panel is hidden correctly when mouse is moved out of the notes panel...");
+		System.out.println("Mouse moved to centre of screen, waiting 2 seconds");
+		System.out.println("notes panel hidden correctly.");
 	}
 }
