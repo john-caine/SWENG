9e4327ed7466ad1a4225f23493396382a27d287c
pmathema <pm670@york.ac.uk>
2014-05-15 16:16:05 +0100

    Controls slide
    Update on Controls slide  panel now showing correctly at the bottom of
    screen

--- Code/eCook/src/eCook/Controls.java
+++ Code/eCook/src/eCook/Controls.java
@@ -1,96 +1,76 @@
-/*
- * Programmer: Steve Thorpe, Paul Mathema.
- * Date Created: 12/05/2014
- * Description: Creates new Control slide with a panel of control buttons which slides in and out from the bottom.
+package eCook;
+/* Title: NotesGUI
  * 
+ * Programmers: Max, Ankita
+ * 
+ * Date Created: 09/05/14
+ * 
+ * Description: A basic GUI to display user-made notes about a particular recipe slide
+ * 				The GUI allows the user to write, read and save notes.
  */
 
-
-package eCook;
-
-import imagehandler.ImageHandler;
 import java.awt.MouseInfo;
-import java.awt.Panel;
-import java.util.ArrayList;
-import audiohandler.AudioHandler;
-import texthandler.TextHandler;
-import videohandler.VideoPlayerHandler;
-import graphicshandler.GraphicsHandler;
-import javafx.animation.*;
-import javafx.application.Application;
-import javafx.event.*;
+import java.awt.Point;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
-import javafx.scene.*;
-import javafx.scene.control.*;
+import javafx.scene.Group;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.input.InputEvent;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.*;
+import javafx.scene.layout.HBox;
 import javafx.stage.Screen;
-import javafx.stage.Stage;
 import javafx.util.Duration;
 
-/**
- * Example of a sidebar that slides in and out of view
- */
 public class Controls {
 	
+	// declare variables
+	boolean controlPanelVisible = false;
+	
+	
+	private Button nextButton;
+	private Button exitButton;
+	private HBox controlPanel;
+	private Button playButton;
+	private Button pauseButton;
+	private Button previousButton;
 	private Rectangle2D primaryScreenBounds;
-	protected HBox panel;
-    protected boolean notesPanelVisible = false;
-    protected Button playButton, pauseButton, nextButton, previousButton, exitButton;
-
-    Label label = new Label();
-
-    // set up the example slide and add the notes panel
-    public void controls (ArrayList<TextHandler> textHandlerList, ArrayList<ImageHandler> imageHandlerList, 
-    						ArrayList<AudioHandler> audioHandlerList, ArrayList<VideoPlayerHandler> videoHandlerList,
-    						Group visibleGroup, Stage primaryStage) {
-        
-    	final StackPane root = new StackPane();
-        final Panel root1 = new Panel();
-
-        // Add a label with sample slide content
-        Label slideText = new Label("Control Panel.");
-
-        // get the size of the screen
-        primaryScreenBounds = Screen.getPrimary().getVisualBounds();
-
-        // Set up the control panel from the Bottom of the screen
-        panel = new HBox();
-        panel.setPrefSize(primaryScreenBounds.getWidth(), primaryScreenBounds.getHeight());
-        //panel.setStyle("-fx-background-color: #336699;");
-        panel.setStyle("-fx-background-color: #DA70D6;");
-        
-        
-        // PUT YOUR CODE HERE TO DECLARE GUI OBJECTS
-        panel.setPadding(new Insets(30, 10, 30, 10));
-        panel.setAlignment(Pos.BOTTOM_CENTER);
-        panel.setSpacing(10);
-        
-        // add buttons to panel
-        playButton();
-        pauseButton();
-        exitButton();
-        previousButton();
-        nextButton();
-        
-        // check to see if the mouse is at the LHS of the screen every time it is moved
-        root.addEventHandler(MouseEvent.MOUSE_MOVED, mouseoverLHSHandler);
-    }
-    
-    public void playButton() {    
-       
+	
+	// constructor
+	public Controls(Integer slideID, Group root) {
+		setupcontrolPanel(slideID, root);
+	}
+	
+	// method to return the controlPanel VBox object
+	public HBox getControlPanel() {
+		return controlPanel;
+	}
+	
+	// method to indicate when the notes panel is onscreen
+	public boolean getcontrolPanelVisible() {
+		return controlPanelVisible;
+	}
+	
+	// method to access the content of the notesBox
+	
+	public void playButton() {    
+	       
         // ADD PlayButton to the Panel
         
            playButton = new Button();
            playButton.setText("'Play'");
         // Add playbutton's event handler and listener   
-           panel.getChildren().add(playButton);
+           controlPanel.getChildren().add(playButton);
                    playButton.setOnAction(new EventHandler<ActionEvent>() {
                        public void handle(ActionEvent e) {
-                           label.setText("Acc");
+                           
                   }
               });
     }
@@ -101,13 +81,9 @@ public class Controls {
             pauseButton = new Button();
             pauseButton.setText("'Pause'");
          // Add pausebutton's event handler and listener      
-            panel.getChildren().add(pauseButton);
-                    playButton.setOnAction(new EventHandler<ActionEvent>() {
-                        public void handle(ActionEvent e) {
-                           // slide.pause();
-                   }
-              });
+            controlPanel.getChildren().add(pauseButton);
                     
+               
     }
              
     public void previousButton() {
@@ -115,88 +91,109 @@ public class Controls {
             previousButton = new Button();
             previousButton.setText("'Pause'");
          // Add previousbutton's event handler and listener               
-            panel.getChildren().add(previousButton);
-                    previousButton.setOnAction(new EventHandler<ActionEvent>() {
-                        public void handle(ActionEvent e) {
-                            label.setText("Acc");
-                   }
-              });
-    }
+            controlPanel.getChildren().add(previousButton);
+                   
+        }
     
-    public void nextButton() {
-                    
+    public void nextButton() {                  
         // Add nextButton to the Panel
            nextButton = new Button();
            nextButton.setText("'Next'");
         // Add nextbutton's event handler and listener                  
-           panel.getChildren().add(nextButton);
-                   nextButton.setOnAction(new EventHandler<ActionEvent>() {
-                       public void handle(ActionEvent e) {
-                             label.setText("Acc");
-                   }
-              });
+           controlPanel.getChildren().add(nextButton);         
     }
     
     public void exitButton() {
                    
         // Add exitButton to the Panel           
-           Button exitButton = new Button();
+            exitButton = new Button();
            exitButton.setText("'Exit'");
         // Add exitbutton's event handler and listener                  
-           panel.getChildren().add(exitButton);
+           controlPanel.getChildren().add(exitButton);
                     exitButton.setOnAction(new EventHandler<ActionEvent>() {
                         public void handle(ActionEvent e) {
-                             label.setText("Acc");
+                             
                      }
              });
     }
-            
 
 
+	// set up the example slide and add the notes panel
+    public void setupcontrolPanel(final Integer slideID, final Group root) {   
+        // get the size of the screen
+        final Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+        
+        // Set up the notes panel on the LHS of the screen
+        controlPanel = new HBox();
+        controlPanel.setLayoutY(primaryScreenBounds.getHeight());
+        controlPanel.setPrefSize(primaryScreenBounds.getWidth(), primaryScreenBounds.getHeight()/5);
+        controlPanel.setStyle("-fx-background-color: #336699;");
+        
+        nextButton();
+        previousButton();
+        exitButton();
+        pauseButton();
+        
+        // clear the help text when the user begins typing
+      
+        
+        final Label timersLabel = new Label("The timers and other stuff could go here");
+        timersLabel.setPadding(new Insets(50,0,0,0));
+        
+        controlPanel.setPadding(new Insets(10,10,10,10));
+        controlPanel.setSpacing(20);
+        controlPanel.setAlignment(Pos.TOP_CENTER);
+        
+
+        
+        // create an instance of the text file handler
+        
+        
         // Define an event handler to trigger when the mouse leaves the notes panel
-        final EventHandler<InputEvent> mouseoutNotesPanelHandler = new EventHandler<InputEvent>() {
-
+        final EventHandler<InputEvent> mouseoutcontrolPanelHandler = new EventHandler<InputEvent>() {
             public void handle(InputEvent event) {
-                if (notesPanelVisible) {
-                    // hide panel
-                    final Timeline timeline = new Timeline();
-                    final KeyValue kv = new KeyValue(panel.translateYProperty(), primaryScreenBounds.getHeight());
-                    final KeyFrame kf = new KeyFrame(Duration.millis(500), kv);
-                    timeline.getKeyFrames().add(kf);
-                    timeline.play();
-                    notesPanelVisible = false;
-                    panel.setDisable(true);
-                }
-                event.consume();
-            }
+            	if (controlPanelVisible) {
+            		// hide panel
+            		final Timeline timeline = new Timeline();
+        			final KeyValue kv = new KeyValue(controlPanel.translateXProperty(), -primaryScreenBounds.getHeight()/5);
+        			final KeyFrame kf = new KeyFrame(Duration.millis(500), kv);
+        			timeline.getKeyFrames().add(kf);
+        			timeline.play();
+                  	controlPanelVisible = false;
+                  	controlPanel.setDisable(true);
+            	}
+            	event.consume();
+            }			
         };
 
         // Define an event handler to trigger when the user moves the mouse
         EventHandler<InputEvent> mouseoverLHSHandler = new EventHandler<InputEvent>() {
-            public void handle(InputEvent event) {
-                // check the position of the mouse
-                java.awt.Point mousePosition = MouseInfo.getPointerInfo().getLocation();
-                // if the mouse is on the far LHS of the screen, show the notes panel
-                if (mousePosition.getX() <= 10) {
-                    if (!panel.getChildren().contains(panel)) {
-                        panel.getChildren().add(panel);
-                        StackPane.setMargin(panel, new Insets(primaryScreenBounds.getHeight(),0,primaryScreenBounds.getHeight()/8,0));
-                        panel.addEventHandler(MouseEvent.MOUSE_EXITED, mouseoutNotesPanelHandler);
-                    }
-                    if (!notesPanelVisible) {
-                        // show panel
-                        final Timeline timeline = new Timeline();
-                        final KeyValue kv = new KeyValue(panel.translateYProperty(), -primaryScreenBounds.getHeight()/100);
-                        final KeyFrame kf = new KeyFrame(Duration.millis(500), kv);
-                        timeline.getKeyFrames().add(kf);
-                        timeline.play();
-                        notesPanelVisible = true;
-                        panel.setDisable(false);
-                    }
-                }
+            public void handle(InputEvent event) {            	
+            	// check the position of the mouse
+            	Point mousePosition = MouseInfo.getPointerInfo().getLocation();           	
+            	// if the mouse is on the far LHS of the screen, show the notes panel
+            	if (mousePosition.getY() >= (primaryScreenBounds.getHeight()-10 )) {
+            		// add the mouselistener
+                    controlPanel.addEventHandler(MouseEvent.MOUSE_EXITED, mouseoutcontrolPanelHandler);
+                    
+                   
+                 
+            		if (!controlPanelVisible) {
+            			// show panel
+            			final Timeline timeline = new Timeline();
+            			final KeyValue kv = new KeyValue(controlPanel.translateYProperty(), primaryScreenBounds.getHeight()/5);
+            			final KeyFrame kf = new KeyFrame(Duration.millis(500), kv);
+            			timeline.getKeyFrames().add(kf);
+            			timeline.play();
+                    	controlPanelVisible = true;
+                    	controlPanel.setDisable(false);
+            		}
+            	}
                 event.consume();
             }
         };
 
+        // check to see if the mouse is at the LHS of the screen every time it is moved
+        root.addEventHandler(MouseEvent.MOUSE_MOVED, mouseoverLHSHandler);
     }
-   
+}
