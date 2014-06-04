012abdb85efe890b30c3616468c977c02aeaccc9
St659 <st659@york.ac.uk>
2014-06-04 18:10:07 +0100

    Slight Change to SlideMediaMock
    Corrected some spelling errors in SlideMediaMock

--- Code/eCook/src/media/SlideMediaMock.java
+++ Code/eCook/src/media/SlideMediaMock.java
@@ -8,29 +8,31 @@ package media;
 
 public class SlideMediaMock extends SlideMedia{
 
-	
-
+	/*
+	 *SlideMediaMock constructor
+	 * @Param xStart: The x co-ordinate of the HBox
+	 * @Param yStart: The y co-ordinate of the HBox
+	 * @Param startTime: The value the start time Timeline is to count from.
+	 */ 
+	 
 	public SlideMediaMock(int xStart, int yStart, Integer startTime) {
 		super(xStart, yStart, startTime);
 		 
 	}
 
+	/*
+	 * Over rides are required for inherited abstract methods. Mocking class does not require an implementation for these methods.
+	 */
 	@Override
 	protected void setTimeLineOnFinish() {
-	
-		
 	}
 
 	@Override
 	protected void tearDown() {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	protected void setTimingValues() {
-		
-		
 	}
 
 }
