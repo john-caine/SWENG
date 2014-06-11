a3d5656493fa69d9e8cb7676fc40bac3669e465a
pmathema <pm670@york.ac.uk>
2014-06-11 18:34:57 +0100

    VideoControl still needs work
    Submitted VideoControl

--- Code/eCook/src/media/VideoControlBarTest.java
+++ Code/eCook/src/media/VideoControlBarTest.java
@@ -1,14 +1,37 @@
 package media;
 
 import static org.junit.Assert.*;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class VideoControlBarTest {
+import eCook.SlideShow;
 
+public class VideoControlBarTest {
+	
+	private VideoControlBar videoControlBar;
+	private VideoHandler videoHandler;
+	private SlideShow parent;
+	
+	@Before
+	public void SetUp(){
+		videoHandler = new VideoHandler(parent, "http://ystv.co.uk/~john.caine/swengrepo" +
+				"/14_Fusion_Promo_spr08.mp4",  60, 50, 100, 100, false, 10, 40);
+		videoControlBar = new VideoControlBar(videoHandler);
+		 Button playPauseButton = new Button("");
+	}
+	
+    /*Test that controlBar has been setup*/
+    
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void setupControlBar() {
+		@SuppressWarnings("unused")
+		HBox controlBar = new HBox();
+		assertNotNull(videoControlBar.playPauseButton);
+		
 	}
 
 }
