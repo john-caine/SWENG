0f54ebfb142285a090a0608375879e16891b58b5
ankitag93 <ankitag93@gmail.com>
2014-06-12 05:28:10 +0100

    eCook visual test for icon

--- Code/eCook/src/eCook/eCookTest.java
+++ Code/eCook/src/eCook/eCookTest.java
@@ -5,30 +5,19 @@
  */
 package eCook;
 
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 
 public class eCookTest {
-	// Run tests on JavaFX thread ref. Andy Till
-	// http://andrewtill.blogspot.co.uk/2012/10/junit-rule-for-javafx-controller-testing.html
-	@Rule public JavaFXThreadingRule javafxRule = new JavaFXThreadingRule();
-	
-	private static Group group;
-	private static Scene scene;
-	
-	@Before
-	// perform JavaFX setup 
-	public void setup() {
-		group = new Group();
-		scene = new Scene(group);
-	}
+
+	/*
+	 * Visual tests
+	 */
 	
 	@Test
 	public void eCookIconCreated(){
-		
+		System.out.println("Testing if eCook icon is created when eCook is run...");
+		System.out.println("Run eCook");
+		System.out.println("Icon is created and displayed correctly.\n");
 	}
 	
 }
