777b42c3fdc1b27a73746db6a768de476f332833
john-caine <Jfc507@york.ac.uk>
2014-06-12 02:49:30 +0100

    notes & recipeHttpAccess code review

--- Code/eCook/src/notes/TextFileHandlerTest.java
+++ Code/eCook/src/notes/TextFileHandlerTest.java
@@ -1,24 +1,19 @@
-package notes;
-
-/* Title: TextFileHandlerTest
- * 
- * Programmers: Max, Ankita
- * 
+/* 
+ *  Programmers: Max, Ankita
  * Date Created: 09/05/14
- * 
  * Description: Testing for CreateTextFile Class. Test specifics are detailed below.
  */
 
-import static org.junit.Assert.*;
+package notes;
 
+import static org.junit.Assert.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import eCook.JavaFXThreadingRule;
 
 public class TextFileHandlerTest {
-	static TextFileHandler handler;
+	protected static TextFileHandler handler;
 	
 	// instantiate the text file handler class before the tests
 	@BeforeClass
@@ -41,7 +36,9 @@ public class TextFileHandlerTest {
 		handler = new TextFileHandler();
 	}
 	
-	// check that no text file is created when null notes String is parsed
+	/**
+	 *  Check that no text file is created when null notes String is parsed
+	 */
 	@Test
 	public void nullNotesTextFileNotCreated() {
 		handler.writeTextFile(null, 5, "Example Recipe Title");
@@ -50,7 +47,9 @@ public class TextFileHandlerTest {
 		assertTrue(handler.error);
 	}
 	
-	// check that no text file is created when null SlideID String is parsed
+	/**
+	 * Check that no text file is created when null SlideID String is parsed
+	 */
 	@Test
 	public void nullSlideIDTextFileNotCreated() {
 		handler.writeTextFile("Example notes", null, "Example Recipe Title");
@@ -59,7 +58,9 @@ public class TextFileHandlerTest {
 		assertTrue(handler.error);
 	}
 	
-	// check that no text file is created when null recipe title String is parsed
+	/** 
+	 * Check that no text file is created when null recipe title String is parsed
+	 */
 	@Test
 	public void nullRcipeTitleTextFileNotCreated() {
 		handler.writeTextFile("Example notes", 1, null);
@@ -68,7 +69,9 @@ public class TextFileHandlerTest {
 		assertTrue(handler.error);
 	}
 	
-	// check that text file is created when valid Strings are parsed
+	/**
+	 * Check that text file is created when valid Strings are parsed
+	 */
 	@Test
 	public void validArgumentsTextFileCreated() {
 		handler.writeTextFile("here are some example notes.", 7, "Example title");
@@ -77,8 +80,10 @@ public class TextFileHandlerTest {
 		assertFalse(handler.error);
 	}
 	
-	// check that the contents of the text file is the same as the notes input String
-	// ensure that any special formatting (\n, \t) is preserved
+	/**
+	 *  Check that the contents of the text file is the same as the notes input String.
+	 *  Ensure that any special formatting (\n, \t) is preserved
+	 */
 	@Test
 	public void textFileContainsCorrectString() {
 		handler.writeTextFile("This is what should be stored in the txt file\nNew Line\ttabbed\nReturn", 8, "notesTestTitle");
@@ -88,7 +93,9 @@ public class TextFileHandlerTest {
 		assertFalse(handler.error);
 	}
 	
-	// further test the read text file method with a null string for a filename
+	/**
+	 *  Further test the read text file method with a null string for a filename
+	 */
 	@Test
 	public void nullFileNameReturnsErrorFromReader() {
 		handler.writeTextFile("example notes", 0, "exampleRecipeTitle0");
@@ -98,7 +105,9 @@ public class TextFileHandlerTest {
 		assertTrue(handler.error);
 	}
 	
-	// check that an error is thrown and nothing is returned when trying to read a file that doesn't exist
+	/**
+	 *  Check that an error is thrown and nothing is returned when trying to read a file that doesn't exist
+	 */
 	@Test
 	public void nonExistentFileReturnsErrorFromReader() {
 		File file = new File(System.getenv("localappdata") + "/eCook/Recipes" + "/nothingHere_1.txt");
