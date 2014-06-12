c94f62a91b42aff868fb47e39d23fef6250b5c7b
ankitag93 <ankitag93@gmail.com>
2014-06-12 05:37:29 +0100

    Re-resolved conflict

--- Code/eCook/src/eCook/eCookTest.java
+++ Code/eCook/src/eCook/eCookTest.java
@@ -5,6 +5,11 @@
  */
 package eCook;
 
+import javafx.scene.Group;
+import javafx.scene.Scene;
+
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
 public class eCookTest {
@@ -12,6 +17,19 @@ public class eCookTest {
 	/*
 	 * Visual tests
 	 */
+
+	// Run tests on JavaFX thread ref. Andy Till
+	// http://andrewtill.blogspot.co.uk/2012/10/junit-rule-for-javafx-controller-testing.html
+	@Rule public JavaFXThreadingRule javafxRule = new JavaFXThreadingRule();
+	
+	private static Group group;
+	@Before
+	// perform JavaFX setup 
+	public void setup() {
+		group = new Group();
+		new Scene(group);
+	}
+
 	
 	@Test
 	public void eCookIconCreated(){
